3460a955df1de499dee33b8786a5bc8a8cc64074
Gameaday <Gameaday@users.noreply.github.com>
2025-10-08 01:28:00 +0000

    deploy: cf35ee5c6e2b77100a8d60198079f79ac39c385c

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -36,9 +36,9 @@ const RESOURCES = {"flutter.js": "888483df48293866f9f41d3d9274a779",
 "canvaskit/canvaskit.js.symbols": "58832fbed59e00d2190aa295c4d70360",
 "canvaskit/skwasm.js.symbols": "0088242d10d7e7d6d2649d1fe1bda7c1",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "2abb0b94a33cc93c0b15fae0099f06b5",
+"flutter_bootstrap.js": "b092c73b19dc863cec00d490ae7b9b78",
 "version.json": "9c1b3a2bb97465d6116b33aedb57d089",
-"main.dart.js": "4a27f2407dc438541db5cef9e51a6b24"};
+"main.dart.js": "9869a6a335091c091637ac6467680488"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
